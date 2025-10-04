078fd9c75f9a5010f330befe10ebb2d4d3c7040a
CTB <baocaothien123@gmail.com4>
2025-10-04 11:56:15 +0700

    [UPDATE] [TOOL] Update map tool - Level management - ongoing

--- Assets/Modules/MapTool/Scripts/Editor/MapToolEditor.cs
+++ Assets/Modules/MapTool/Scripts/Editor/MapToolEditor.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
 using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
+
+#if UNITY_EDITOR
 using UnityEditor;
+using UnityEngine.SceneManagement;
+using System.IO;
+#endif
 
 namespace MapTool
 {
     public class MapToolEditor : OdinMenuEditorWindow
     {
-        [BoxGroup("Cell types settings"), SerializeField]
+        #region Cell types settings
+        [TabGroup("Cell types settings")]
+        [SerializeField]
+        [FolderPath]
         private string dataPath = "Assets/Modules/MapTool/Data/CellTypes";
+        #endregion
+
+        #region Level settings
+        [TabGroup("Level settings")]
+        [AssetsOnly]
+        [Required("Template scene is required")]
+        [SerializeField]
+        [InfoBox("Scene used as a template when creating new levels. It should contain essential components like Grid, Camera setup, Lighting, etc.", InfoMessageType.None)]
+        private SceneAsset templateScene;
+
+        [TabGroup("Level settings")]
+        [FolderPath]
+        [SerializeField]
+        [InfoBox("Path where new level scenes will be created.", InfoMessageType.None)]
+        private string levelPath = "Assets/Scenes/Levels";
+        #endregion
+
 
         private CreateNewCellTypeData createNewCellTypeData;
+        private CreateNewLevelData createNewLevelData;
 
         [MenuItem("Tools/Map Tool")]
         private static void OpenWindow()
@@ -23,7 +49,7 @@ namespace MapTool
         {
             base.OnDestroy();
 
-            if(createNewCellTypeData != null && createNewCellTypeData.cellTypeSO != null)
+            if (createNewCellTypeData != null && createNewCellTypeData.cellTypeSO != null)
             {
                 DestroyImmediate(createNewCellTypeData.cellTypeSO);
             }
@@ -34,11 +60,12 @@ namespace MapTool
             var tree = new OdinMenuTree();
 
             createNewCellTypeData = new CreateNewCellTypeData(dataPath);
+            createNewLevelData = new CreateNewLevelData(templateScene, levelPath);
 
             tree.Add("Cell Types/ Create New Type", createNewCellTypeData);
             tree.AddAllAssetsAtPath("Cell Types/ All Cell Types", dataPath, typeof(CellTypeSO));
 
-            tree.Add("Level editor/ Create New Level", new CreateNewLevelData());
+            tree.Add("Level editor/ Create New Level", createNewLevelData);
 
             tree.Add("Settings", this);
             return tree;
@@ -73,9 +100,85 @@ namespace MapTool
 
     public class CreateNewLevelData
     {
-        public CreateNewLevelData()
+        [SerializeField]
+        [ValidateInput("ValidateLevelName", "Level name is required and must contain valid characters!")]
+        private string levelName;
+
+        private SceneAsset sceneTemplate;
+        private string levelPath;
+        public CreateNewLevelData(SceneAsset sceneTemplate, string levelPath)
         {
-            
+            this.sceneTemplate = sceneTemplate;
+            this.levelPath = levelPath;
         }
+
+#if UNITY_EDITOR
+        [HorizontalGroup("Actions")]
+        [Button("Create level scene", ButtonSizes.Large), GUIColor(0.4f, 1f, 0.4f)]
+        private void CreateLevel()
+        {
+            if (!ValidateInputs())
+                return;
+
+            string newScenePath = $"{levelPath}/{levelName}.unity";
+
+            if (File.Exists(newScenePath))
+            {
+                if (!EditorUtility.DisplayDialog(
+                    "Scene already exist",
+                    $"A scene name '{levelName} already exist at: \n{newScenePath}'",
+                    "Override",
+                    "Cancel"))
+                {
+                    return;
+                }
+
+                AssetDatabase.DeleteAsset(newScenePath);
+            }
+
+            if(!Directory.Exists(levelPath))
+            {
+                Directory.CreateDirectory(levelPath);
+                AssetDatabase.Refresh();
+            }
+        }
+        private bool ValidateLevelName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return false;
+
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            return name.IndexOfAny(invalidChars) == -1;
+        }
+
+        private bool ValidateInputs()
+        {
+            if (sceneTemplate == null)
+            {
+                EditorUtility.DisplayDialog("Error", "Template scene is required!", "OK");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(levelName))
+            {
+                EditorUtility.DisplayDialog("Error", "Level name cannot be empty!", "OK");
+                return false;
+            }
+
+            if (!ValidateLevelName(levelName))
+            {
+                EditorUtility.DisplayDialog("Error", "Level name contains invalid characters!", "OK");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(levelPath) || !levelPath.StartsWith("Assets/"))
+            {
+                EditorUtility.DisplayDialog("Error", "Level path must be valid and start with 'Assets/'!", "OK");
+                return false;
+            }
+
+            return true;
+        }
+#endif
     }
 }
