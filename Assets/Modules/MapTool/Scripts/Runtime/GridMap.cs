66a3f38bd1430c54cbe7d7dd1230df19a86ddd78
CTB <baocaothien123@gmail.com4>
2025-10-17 03:12:38 +0700

    [UPDATE] Basic grid setup

--- Assets/Modules/MapTool/Scripts/Runtime/GridMap.cs
+++ Assets/Modules/MapTool/Scripts/Runtime/GridMap.cs
@@ -4,6 +4,30 @@ namespace MapTool
 {
     public class GridMap : MonoBehaviour
     {
+        [SerializeField] private Canvas gridCanvas;
+        [SerializeField] private Transform gridParent;
 
+        [SerializeField] private Vector3 originPosition;
+
+        private FN.Grid grid;
+        private void Start()
+        {
+            grid = new FN.Grid(10, 10, 1f, canvas: gridCanvas, gridParent: gridParent, originPosition: originPosition);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                grid.SetCellValue(worldPosition, 1);
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Debug.Log(grid.GetCellValue(worldPosition));
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Utils/FNUtils.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace FN
+{
+    public class FNUtils
+    {
+
+    }
+}
